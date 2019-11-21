8d151744e2ca66b5669f0d9b1c2618076eee4f12
randika123 <rperera@mitrai.com>
2019-11-21 08:27:33 +0700

    logger module

---
+++ LoggerModule/LoggerMediator/src/main/java/com/cellcard/wso2/logger/LogFields.java
@@ -0,0 +1,260 @@
+package com.cellcard.wso2.logger;
+
+public class LogFields {
+	
+	//existing log fields
+	private String vasAppFormate;
+	private String user ;
+    private String accountId;
+    private String sourceAccountId;
+    private String targetAccountId;
+    private String saleId;
+    private String requestId;
+    private String serverHost;
+    private String clientIp;
+    private String methodName;
+    private String requestPlan;
+    private String serviceName;
+    private String channel;
+    private String purchaseFee;
+    private String uuid;
+    private String step;
+    private String nei;
+    private String api;
+    private String action;
+    private String result;
+    private String errorCode;
+    private String errorMsg;
+    private String transactionPlan;
+    private String tariffPlanCospid;
+    private String classOfServiceid;
+    private String oldPlan;
+	//new log fields
+    private String logLevel;
+    private String contextPath;
+    private String sequenceName;
+    private String logPoint;
+    private String logMessage;
+    private String errorMessage;
+    private String errorReason;
+    private String messageBody;
+    private String originatingErrorMessage;
+    private String originatingErrorDetail;
+	public String getVasAppFormate() {
+		return vasAppFormate;
+	}
+	public void setVasAppFormate(String vasAppFormate) {
+		this.vasAppFormate = vasAppFormate;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public String getSourceAccountId() {
+		return sourceAccountId;
+	}
+	public void setSourceAccountId(String sourceAccountId) {
+		this.sourceAccountId = sourceAccountId;
+	}
+	public String getTargetAccountId() {
+		return targetAccountId;
+	}
+	public void setTargetAccountId(String targetAccountId) {
+		this.targetAccountId = targetAccountId;
+	}
+	public String getSaleId() {
+		return saleId;
+	}
+	public void setSaleId(String saleId) {
+		this.saleId = saleId;
+	}
+	public String getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+	public String getServerHost() {
+		return serverHost;
+	}
+	public void setServerHost(String serverHost) {
+		this.serverHost = serverHost;
+	}
+	public String getClientIp() {
+		return clientIp;
+	}
+	public void setClientIp(String clientIp) {
+		this.clientIp = clientIp;
+	}
+	public String getMethodName() {
+		return methodName;
+	}
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	public String getRequestPlan() {
+		return requestPlan;
+	}
+	public void setRequestPlan(String requestPlan) {
+		this.requestPlan = requestPlan;
+	}
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	public String getChannel() {
+		return channel;
+	}
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+	public String getPurchaseFee() {
+		return purchaseFee;
+	}
+	public void setPurchaseFee(String purchaseFee) {
+		this.purchaseFee = purchaseFee;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public String getStep() {
+		return step;
+	}
+	public void setStep(String step) {
+		this.step = step;
+	}
+	public String getNei() {
+		return nei;
+	}
+	public void setNei(String nei) {
+		this.nei = nei;
+	}
+	public String getApi() {
+		return api;
+	}
+	public void setApi(String api) {
+		this.api = api;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public String getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+	public String getTransactionPlan() {
+		return transactionPlan;
+	}
+	public void setTransactionPlan(String transactionPlan) {
+		this.transactionPlan = transactionPlan;
+	}
+	public String getTariffPlanCospid() {
+		return tariffPlanCospid;
+	}
+	public void setTariffPlanCospid(String tariffPlanCospid) {
+		this.tariffPlanCospid = tariffPlanCospid;
+	}
+	public String getClassOfServiceid() {
+		return classOfServiceid;
+	}
+	public void setClassOfServiceid(String classOfServiceid) {
+		this.classOfServiceid = classOfServiceid;
+	}
+	public String getOldPlan() {
+		return oldPlan;
+	}
+	public void setOldPlan(String oldPlan) {
+		this.oldPlan = oldPlan;
+	}
+	public String getLogLevel() {
+		return logLevel;
+	}
+	public void setLogLevel(String logLevel) {
+		this.logLevel = logLevel;
+	}
+	public String getContextPath() {
+		return contextPath;
+	}
+	public void setContextPath(String contextPath) {
+		this.contextPath = contextPath;
+	}
+	public String getSequenceName() {
+		return sequenceName;
+	}
+	public void setSequenceName(String sequenceName) {
+		this.sequenceName = sequenceName;
+	}
+	public String getLogPoint() {
+		return logPoint;
+	}
+	public void setLogPoint(String logPoint) {
+		this.logPoint = logPoint;
+	}
+	public String getLogMessage() {
+		return logMessage;
+	}
+	public void setLogMessage(String logMessage) {
+		this.logMessage = logMessage;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	public String getErrorReason() {
+		return errorReason;
+	}
+	public void setErrorReason(String errorReason) {
+		this.errorReason = errorReason;
+	}
+	public String getMessageBody() {
+		return messageBody;
+	}
+	public void setMessageBody(String messageBody) {
+		this.messageBody = messageBody;
+	}
+	public String getOriginatingErrorMessage() {
+		return originatingErrorMessage;
+	}
+	public void setOriginatingErrorMessage(String originatingErrorMessage) {
+		this.originatingErrorMessage = originatingErrorMessage;
+	}
+	public String getOriginatingErrorDetail() {
+		return originatingErrorDetail;
+	}
+	public void setOriginatingErrorDetail(String originatingErrorDetail) {
+		this.originatingErrorDetail = originatingErrorDetail;
+	}
+    
+}
